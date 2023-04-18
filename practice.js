87f3b4ba821426d19b403fa04487fcc0ba6a33c0
Ion Stici <ionsticidev@gmail.com>
2023-04-18 11:36:33 +0300

    Update Linked List Swapping Elements

--- practice.js
+++ practice.js
@@ -1,35 +1,3 @@
-[&larr; Back](./README.md)
-
-# Singly Linked Lists
-
-- [Linked List (javascript-algorithms repo)](https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/linked-list)
-
-**Singly Linked Lists** are a linear data structure (unidirectional) where elements are stored in nodes, and each node points to the next node in the sequence.
-
-A **linked list** is comprised of a sequential chain of nodes, where each node contains two items, the _Value_ and the _Reference_ to the next node.
-
-- Linked Lists have a single **head** node, which serves as the first node in the list.
-- Then the last node is referred as the **tail**, denoted by the pointer set to `null`.
-
-_Methods a linked list should provide:_
-
-1. ADD new node to the beginning or to the end of the list
-2. REMOVE a node from the beginning of the list
-3. TRAVERSING the linked list + PRINT out the nodes in order
-
-## Adding a new node to head
-
-Link the new node to the current head node.
-
-## Removing a node
-
-To remove a node, we reset the pointers that point to the node we want to remove, so that it will become an "Orphaned node".
-
-<br>
-
-## Constructor and Methods
-
-```js
 class Node {
   constructor(data) {
     this.data = data;
@@ -45,9 +13,7 @@ class Node {
     return this.next;
   }
 }
-```
 
-```js
 class LinkedList {
   constructor() {
     this.head = null;
@@ -80,7 +46,7 @@ class LinkedList {
   }
 
   printList() {
-    const currentNode = this.head;
+    let currentNode = this.head;
     let output = "<head> ";
     while (currentNode !== null) {
       output += currentNode.data + " ";
@@ -91,28 +57,8 @@ class LinkedList {
   }
 }
 
-const list = new LinkedList();
-list.addToHead(2);
-list.addToHead(1);
-list.addToTail(3);
-list.printList(); // 1 2 3
-```
+// // // // // // // // // // // // // // // // // // // //
 
-<br>
-
-## Swapping Elements in a Linked List
-
-Swap `data1` and `data2` nodes in a linked list.
-
-1. Iterate through the list looking for the nodes that matches `data1` and `data2` to be swapped (`node1` and `node2`). Keep track of the node's previous node as you iterate `node1Prev` and `node2Prev`.
-2. If `node1Prev` is `null`, `node1` was the head of the list, so set the list’s head to `node2`
-3. Otherwise, set `node1Prev`‘s next node to `node2`
-4. If `node2Prev` is `null`, set the list’s head to `node1`
-5. Otherwise, set `node2Prev`‘s next node to `node1`
-6. Set `node1`‘s next node to `node2`‘s next node
-7. Set `node2`‘s next node to `node1`‘s next node
-
-```js
 const list = new LinkedList();
 
 for (let i = 0; i <= 10; i++) {
@@ -179,6 +125,5 @@ function swapNodes(list, data1, data2) {
   node1.setNextNode(node2.getNextNode());
   node2.setNextNode(temp);
 }
-```
 
-<br>
+// // // // // // // // // // // // // // // // // // // //
